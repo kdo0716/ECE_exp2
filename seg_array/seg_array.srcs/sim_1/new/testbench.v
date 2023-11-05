87fff5902571c491ba07251ce3e22a5a55555b16
kdo0716 <144528643+kdo0716@users.noreply.github.com>
2023-11-05 21:42:25 +0900

    4.3

--- seg_array/seg_array.srcs/sim_1/new/testbench.v
+++ seg_array/seg_array.srcs/sim_1/new/testbench.v
@@ -4,37 +4,55 @@ module testbench();
 
 reg clk, rst;
 reg btn;
-reg [3:0] state_bin;
-wire [7:0] state_bcd;
+//reg [3:0] state_bin;
+//wire [7:0] state_bcd;
 wire [7:0] seg_data;
 wire [7:0] seg_sel;
-wire [3:0] bcd;
+//reg [3:0] bcd;
 
 seg_array tb(clk, rst, btn, seg_data, seg_sel);
 
 initial begin
     clk <= 0;
     rst <= 1;
-    state_bin <= 4'b0000;
+    btn <= 0;
     #10 rst <= 0;
     #10 rst <= 1;
-    #30 state_bin <= 4'b0001;
-    #30 state_bin <= 4'b0010;
-    #30 state_bin <= 4'b0011;
-    #30 state_bin <= 4'b0100;
-    #30 state_bin <= 4'b0101;
-    #30 state_bin <= 4'b0110;
-    #30 state_bin <= 4'b0111;
-    #30 state_bin <= 4'b1000;
-    #30 state_bin <= 4'b1001;
-    #30 state_bin <= 4'b1010;
-    #30 state_bin <= 4'b1011;
-    #30 state_bin <= 4'b1100;
-    #30 state_bin <= 4'b1101;
-    #30 state_bin <= 4'b1110;
-    #30 state_bin <= 4'b1111;
-//    #30 state_bin <= 4'b0001;
-end
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    #35 btn <= 1;
+    #35 btn <= 0;
+    end
 
 always begin 
     #5 clk <= ~clk;
